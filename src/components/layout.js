00e932c6efc1590ee8d219243f8859f5edcf75d4
Alberto Leal <mail4alberto@gmail.com>
2019-03-18 00:23:18 -0400

    default

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,49 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-
+import { Helmet } from "react-helmet";
 import styled, { createGlobalStyle } from "styled-components";
 import "normalize.css";
 
 // component
 
+// NOTE: these are defaults from the RBC design system
+const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #fff;
+  }
+
+  body {
+    font-family: "Roboto", "Helvetica", sans-serif;
+    font-size: 16px;
+    font-weight: 300;
+    letter-spacing: 0px;
+    line-height: 1.5;
+  }
+
+`;
+
+const Styles = () => {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <GlobalStyle />
+    </React.Fragment>
+  );
+};
+
 const Layout = ({ children }) => {
-  return <React.Fragment>{children}</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Styles />
+      {children}
+    </React.Fragment>
+  );
 };
 
 Layout.propTypes = {
